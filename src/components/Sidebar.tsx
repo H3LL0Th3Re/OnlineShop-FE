5f80251e3d1a4f2bc44cd80425ab6b5d389ac373
mujtahidul08 <mujahaq@gmail.com>
2025-01-22 20:20:02 +0700

    chore: clean ui sidebar, layout and navbar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,7 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, HStack, Spacer, Text } from '@chakra-ui/react';
+import { FiBox } from 'react-icons/fi';
+import { RiDashboardHorizontalLine } from 'react-icons/ri';
+import { TbSettings, TbSmartHome } from 'react-icons/tb';
 import { Link } from 'react-router';
 
 const Sidebar = () => {
@@ -10,30 +13,46 @@ const Sidebar = () => {
       p="4"
       justify="space-between"
       align="center"
+      h="full"
+      
     >
-      <Flex gap="4" direction={'column'} w={'70%'}>
-        <Text color="#5F2EEA" fontWeight="900" fontSize="25px" mb="0">
+      <Flex gap="4" direction={'column'} w={'80%'}  h="full">
+        <Text color="#5F2EEA" fontWeight="900" fontSize="25px" mb="0" >
           LAKOE APP
         </Text>
         <Link to="/">
-          <Text p={1} _hover={{ bgColor: 'grey' }} borderRadius={5}>
-            Dashboard
-          </Text>
+          <HStack _hover={{ color: '#5F2EEA' }}>
+            <TbSmartHome />
+            <Text p={1}  borderRadius={5} fontWeight="600">
+              Dashboard
+            </Text>
+          </HStack>
         </Link>
         <Link to="/product">
-          <Text p={1} _hover={{ bgColor: 'grey' }} borderRadius={5}>
-            Produk
+        <HStack _hover={{ color: '#5F2EEA' }} align="center">
+        <RiDashboardHorizontalLine />
+          <Text p={1}  borderRadius={5} fontWeight="600">
+            Products
           </Text>
+        </HStack>
         </Link>
         <Link to="/order">
-          <Text p={1} _hover={{ bgColor: 'grey' }} borderRadius={5}>
-            Pesanan
+        <HStack _hover={{ color: '#5F2EEA' }} align="center">
+        <FiBox />
+          <Text p={1} borderRadius={5} fontWeight="600">
+            Orders
           </Text>
+        </HStack>
         </Link>
+
+        <Spacer />
         <Link to="/pengaturan">
-          <Text p={1} _hover={{ bgColor: 'grey' }} borderRadius={5}>
-            Pengaturan
+        <HStack _hover={{ color: '#5F2EEA' }}>
+        <TbSettings />
+          <Text p={1} borderRadius={5} fontWeight="600">
+            Settings
           </Text>
+        </HStack>
         </Link>
       </Flex>
     </Flex>
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -8,12 +8,12 @@ const Layout = () => {
     <Box>
       {/* <Navbar/> */}
       <Flex>
-        <Box w={'20%'} display="">
+        <Box w={'20%'} h="100vh" position="sticky" top="0" left="0" overflow="auto" zIndex="1">
           <Sidebar />
         </Box>
 
         <Box w={'80%'} p={0} bg={'blackAlpha.100'}>
-          <Box>
+          <Box position="sticky" top="0" right="0" overflow="auto" zIndex="2">
             <Navbar />
           </Box>
           <Box p={3}>
