b0aff72cab41fed6741a88c1ada7bbb7483f574c
RizkiRahmadani <rzkrhmdn7@gmail.com>
2025-01-22 10:51:25 +0700

    feat: add routing login & register

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Fallback from './pages/fallback';
 import Product from './pages/product';
 import { Dashboard } from './pages/dashboard';
 import addProduct from './pages/addProduct';
+import Login from './pages/login';
+import Registration from './pages/registration';
 
 function App() {
   const router = createBrowserRouter([
@@ -20,6 +22,21 @@ function App() {
         {
           path: '/product',
           Component: Product,
+          HydrateFallback: Fallback,
+        },
+        {
+          path: '/add-product',
+          Component: addProduct,
+          HydrateFallback: Fallback,
+        },
+        {
+          path: '/login',
+          Component: Login,
+          HydrateFallback: Fallback,
+        },
+        {
+          path: '/register',
+          Component: Registration,
           HydrateFallback: Fallback,
         },
         {
