3d434964ac1a8095e87dff7e414b2658cc88ef47
RizkiRahmadani <rzkrhmdn7@gmail.com>
2025-01-22 11:01:48 +0700

    feat: add button form create product

--- src/components/ListProduct.tsx
+++ src/components/ListProduct.tsx
@@ -11,6 +11,7 @@ import {
 import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from './ui/menu';
 import { Switch } from './ui/switch';
 import { BsArrowUp } from 'react-icons/bs';
+import { Link } from 'react-router';
 
 const ListProduct = () => {
   return (
@@ -21,7 +22,9 @@ const ListProduct = () => {
           <Text fontSize="2xl" fontWeight="bold">
             Daftar Produk
           </Text>
-          <Button>Tambah Produk</Button>
+          <Box bg={'blue.400'} p={3} rounded={'md'}>
+            <Link to={'/add-product'}>Tambah Produk</Link>
+          </Box>
         </Flex>
 
         <Flex mb="4" gap="4">
@@ -56,9 +59,8 @@ const ListProduct = () => {
                 borderColor="gray.200"
                 rounded="md"
               >
-                <Box bg={'black'}>
+                <Box bg={'black'} rounded="md">
                   <Image
-                    rounded="md"
                     src="https://ecs7.tokopedia.net/img/product-1/2015/8/30/574846/574846_bc62bae2-ce97-489d-bfcc-4c14ec8d7ec1.jpg"
                     alt="Navy"
                     w={20}
@@ -75,13 +77,13 @@ const ListProduct = () => {
                   </Box>
                   <Flex align="center" justify="space-between">
                     <Flex gap={4}>
-                      <Button size="sm" colorScheme="blue">
+                      <Button size="sm" bg="blue">
                         Ubah Harga
                       </Button>
-                      <Button size="sm" colorScheme="green">
+                      <Button size="sm" bg="green">
                         Ubah Stok
                       </Button>
-                      <Button size="sm" colorScheme="gray">
+                      <Button size="sm" bg="gray">
                         Lihat Halaman
                       </Button>
                     </Flex>
