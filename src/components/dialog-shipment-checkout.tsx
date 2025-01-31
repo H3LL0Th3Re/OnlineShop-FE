d5706f8eeb803cfee120d71b13a0e7d068a24fba
RizkiRahmadani <rzkrhmdn7@gmail.com>
2025-01-31 09:30:56 +0700

    chore: delete unused vars

--- src/components/dialog-shipment-checkout.tsx
+++ src/components/dialog-shipment-checkout.tsx
@@ -1,11 +1,9 @@
 import { Box, HStack, Image, Text, VStack } from '@chakra-ui/react';
 import { Button } from './ui/button';
 import {
-  DialogActionTrigger,
   DialogBody,
   DialogCloseTrigger,
   DialogContent,
-  DialogFooter,
   DialogHeader,
   DialogRoot,
   DialogTitle,
@@ -46,8 +44,7 @@ const couriers = [
 ];
 
 export default function DialogShipmentCheckout() {
-
-     const [selectedShipment, setSelectedShipment] = useState<string | null>(null);
+  const [selectedShipment, setSelectedShipment] = useState<string | null>(null);
   return (
     <DialogRoot>
       <DialogTrigger asChild>
@@ -57,31 +54,53 @@ export default function DialogShipmentCheckout() {
         <DialogHeader>
           <DialogTitle>Select Your Shipment</DialogTitle>
         </DialogHeader>
-       
-          <DialogBody >
+
+        <DialogBody>
           {couriers.map((courier) => (
-            <Box  gap="1" mb="8px" p="1"
-            onClick={() => setSelectedShipment(courier.name)}
-            cursor="pointer"
-            bg={selectedShipment === courier.name ? 'blue.100' : 'white'} 
-            borderWidth="1px"
-            borderColor={selectedShipment === courier.name ? 'blue.500' : 'gray.200'} 
-            borderRadius="8px"
+            <Box
+              gap="1"
+              mb="8px"
+              p="1"
+              onClick={() => setSelectedShipment(courier.name)}
+              cursor="pointer"
+              bg={selectedShipment === courier.name ? 'blue.100' : 'white'}
+              borderWidth="1px"
+              borderColor={
+                selectedShipment === courier.name ? 'blue.500' : 'gray.200'
+              }
+              borderRadius="8px"
             >
-              <HStack  w="full">
-                <Image src={courier.image} h="100px" w="100px" borderRadius="8px"/>
+              <HStack w="full">
+                <Image
+                  src={courier.image}
+                  h="100px"
+                  w="100px"
+                  borderRadius="8px"
+                />
                 <VStack w="full">
-                  <Text w="full" fontSize="17px"> {courier.name}</Text>
-                  <Text w="full" color="grey"> {courier.type}</Text>
+                  <Text w="full" fontSize="17px">
+                    {' '}
+                    {courier.name}
+                  </Text>
+                  <Text w="full" color="grey">
+                    {' '}
+                    {courier.type}
+                  </Text>
                 </VStack>
-                <Text w="full" textAlign="right" fontSize="17px" fontWeight="600"> Rp {formatPrice(courier.price)}</Text>
+                <Text
+                  w="full"
+                  textAlign="right"
+                  fontSize="17px"
+                  fontWeight="600"
+                >
+                  {' '}
+                  Rp {formatPrice(courier.price)}
+                </Text>
               </HStack>
             </Box>
-            ))}
-          </DialogBody>
-        
+          ))}
+        </DialogBody>
 
-        
         <DialogCloseTrigger />
       </DialogContent>
     </DialogRoot>
