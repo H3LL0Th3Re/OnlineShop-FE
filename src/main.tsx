001ceb10fe7d9b2876c71fa12dbec69f3825caf3
RizkiRahmadani <rzkrhmdn7@gmail.com>
2025-02-24 13:32:41 +0700

    add logo

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { Provider } from './components/ui/provider.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')!).render(
       <Provider>
         <App />
       </Provider>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </StrictMode>
 );
