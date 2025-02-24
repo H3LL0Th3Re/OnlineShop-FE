e05d0a9c49f2e3d78e27a826e0933b0525df700c
RizkiRahmadani <rzkrhmdn7@gmail.com>
2025-02-24 11:43:35 +0700

    fix: dialog data buyer

--- src/components/dialog-data-buyer.tsx
+++ src/components/dialog-data-buyer.tsx
@@ -22,7 +22,7 @@ import {
 } from '@/components/ui/dialog';
 import { useEffect, useState } from 'react';
 import { Field } from '@/components/ui/field';
-import { currentStore } from '@/features/get-store';
+
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { apiURL } from '@/utils/api-url';
@@ -64,7 +64,7 @@ export const DialogDataBuyer = () => {
       if (token) {
         try {
           const response = await fetch(
-            `${apiURL}/api/locations/api/provinces`,
+            `http://localhost:3000/api/locations/api/provinces`,
             {
               method: 'GET',
               headers: {
@@ -96,7 +96,7 @@ export const DialogDataBuyer = () => {
       if (provinceCode && token) {
         try {
           const response = await fetch(
-            `${apiURL}/locations/api/cities/${provinceCode}`,
+            `http://localhost:3000/api/locations/api/cities/${provinceCode}`,
             {
               method: 'GET',
               headers: {
@@ -133,7 +133,7 @@ export const DialogDataBuyer = () => {
       if (cityCode && token) {
         try {
           const response = await fetch(
-            `${apiURL}/locations/api/districts/${cityCode}`,
+            `http://localhost:3000/api/locations/api/districts/${cityCode}`,
             {
               method: 'GET',
               headers: {
@@ -171,7 +171,7 @@ export const DialogDataBuyer = () => {
       if (districtCode && token) {
         try {
           const response = await fetch(
-            `${apiURL}/locations/api/villages/${districtCode}`,
+            `http://localhost:3000/api/locations/api/villages/${districtCode}`,
             {
               method: 'GET',
               headers: {
@@ -205,11 +205,6 @@ export const DialogDataBuyer = () => {
     }
   }, [formData.district, token]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
-
   useEffect(() => {
     const productData = localStorage.getItem('selectedProduct');
     if (productData) {
@@ -217,10 +212,14 @@ export const DialogDataBuyer = () => {
     }
   }, []);
 
+  const product_id = product?.id || '';
+  const product_height = product?.height;
+  const product_width = product?.width;
+  const product_length = product?.length;
+  const product_weight = product?.weight;
   const productName = product?.name || ''; // Default to empty string if no product
   const price = product?.price || 0;
   const quantity = product?.quantity || 1;
-
 
   // console.log('id produk', product_id);
 
@@ -236,57 +235,59 @@ export const DialogDataBuyer = () => {
 
     // console.log(response);
     return response.data;
-
   }
-  const response_location = location_fetch();
+  const stores = fetch_store_location();
 
-  async function user_fetch() {
-    const user_response = await axios.get(apiURL + '/user', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    });
-    return user_response.data.user[0];
-  }
-  const response_user = user_fetch();
+  // async function location_fetch(){
+  //   const location_response = await axios.get(apiURL + '/locations',{
 
+  //     headers:{
+  //       "Authorization": `Bearer ${token}`,
+  //       "Content-Type": "application/json"
+  //     }
+  //   })
+
+  //   // console.log("lokasi store",location_response.data.location[0])
+  //   return location_response.data.location[0]
+  // }
+  // const response_location = location_fetch();
   async function onSubmit(
     productName: string,
     price: number,
     quantity: number
   ) {
     console.log('Form Data Before Sending:', formData);
-    console.log('Destination Email:', formData.email);
+    // console.log("Destination name:", (await response_user));
 
     const order_response = await axios.post(apiURL + '/order/add-order', {
-      origin_contact_name: (await store_response).name,
-      origin_contact_phone: (await response_user).phone_number,
-      origin_contact_email: (await response_user).email,
-      origin_address: `${(await response_location).city_district}, ${(await response_location).address}`,
-      origin_postal_code: (await response_location).postal_code,
-      destination_contact_email: formData.email,
+      origin_contact_name: (await storeUser_fetch).fullname,
+      origin_contact_phone: (await storeUser_fetch).phone_number,
+      origin_contact_email: (await storeUser_fetch).email,
+      origin_address: `${(await stores).location_store[0].city_district}, ${(await stores).location_store[0].address}`,
+
+      origin_postal_code: (await stores).location_store[0].postal_code,
       destination_contact_name: formData.name,
       destination_contact_phone: formData.phone_number,
+      destination_contact_email: formData.email,
       destination_address: `${formData.detail_address}, ${getLabelByValue(provinces, formData.province)}, ${getLabelByValue(districts, formData.district)}, ${getLabelByValue(villages, formData.sub_district)}, ${getLabelByValue(cities, formData.city)}`,
       destination_postal_code: formData.postal_code,
+
       delivery_type: 'now',
       items: [
         {
           name: productName,
-          description: 'cow is sacred saar',
+
           value: price,
           quantity: quantity,
-          height: 200,
-          length: 200,
-          weight: 200,
-          width: 200,
+          height: product_height,
+          length: product_length,
+          weight: product_weight,
+          width: product_width,
         },
       ],
     });
 
     localStorage.setItem('order_id_response', order_response.data.orderId);
-
     console.log('response dari order bro', order_response.data);
   }
 
@@ -296,9 +297,31 @@ export const DialogDataBuyer = () => {
     });
     // console.log('user res', response);
     return response.data.user;
-
-    console.log(JSON.stringify(order_response.data));
   }
+  const storeUser_fetch = user_store_fetch();
+
+  // async function user_fetch(){
+  //   const user_response = await axios.get(apiURL + '/user', {
+  //     headers:{
+  //       "Authorization": `Bearer ${token}`,
+  //       "Content-Type": "application/json"
+  //     }
+  //   })
+  //   return user_response.data.user[0];
+  // }
+  // const response_user = user_fetch();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  useEffect(() => {
+    const productData = localStorage.getItem('selectedProduct');
+    if (productData) {
+      setProduct(JSON.parse(productData)); // Set the product from localStorage
+    }
+  }, []);
 
   const [open, setOpen] = useState(false);
   return (
