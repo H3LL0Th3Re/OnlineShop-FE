7321d73cd90a4b0a502d19b7864d42a810911a21
mujtahidul08 <mujahaq@gmail.com>
2025-02-24 12:52:06 +0700

    style: register and login

--- src/components/Settings/Dialog/dialog-add-location-settings.tsx
+++ src/components/Settings/Dialog/dialog-add-location-settings.tsx
@@ -24,7 +24,6 @@ import L from 'leaflet';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-import { apiURL } from '@/utils/api-url';
 import { FaLocationDot } from 'react-icons/fa6';
 import { apiURL } from '@/utils/api-url';
 
@@ -274,9 +273,7 @@ export default function DialogAddLocation() {
   }
 
   const addLocation = async (data: LocationData) => {
-
     const response = await axios.post(apiURL + '/locations/create', data, {
-
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`,
